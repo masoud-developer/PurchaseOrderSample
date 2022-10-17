533b2ed5e556cfbb6e86f2ee36ddcd92b208dc2c
masoud <masoud@drethos.com>
2022-10-18 00:36:42 +0300

    ...

--- Backend/Business/DataAccess/AppDbContext.cs
+++ Backend/Business/DataAccess/AppDbContext.cs
@@ -7,7 +7,7 @@ namespace Business.DataAccess;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext()
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
